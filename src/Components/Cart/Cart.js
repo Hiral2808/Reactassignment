4d562af5ccaca6448016be7c25dffe4d74519abf
Hiral2808 <hiru.prajapati2000@gmail.com>
2022-03-17 17:15:43 +0530

    this is my second commit

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,51 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "../Navbar/Navbar";
 import "../About/About.css";
 import { Card } from "react-bootstrap";
 import { cartitems } from "../Product/Product";
 const Cart = () => {
   console.log(cartitems);
-  const [cartdata, newCartdata] = useState(cartitems);
-  console.log(cartdata);
   
 
   return (
+  
     <div>
       <Navbar />
       <div className="abouts">Cart page </div>
       <div>
-        {cartdata.map((cartitems) => {
+        {cartitems.map((cartitems) => {
           return (
-            <div key={cartitems.id}>
-              <div>
-                <Card
-                  style={{
-                    width: "18rem",
-                    display:"flex",
-                    backgroundColor: "#f8f8f8",
-                    margin: "120px",
-                    marginLeft: "40px",
-                    marginBottom: "-20px",
-                    padding: "10px",
-                  }}
-                >
-                  <Card.Img
-                    variant="top"
-                    src={cartitems.image}
-                    style={{ height: "300px" }}
-                  />
-                  <Card.Body>
-                    <Card.Title>
-                      {cartitems.name} {cartitems.price}
-                    </Card.Title>
-                  </Card.Body>
-                </Card>
-              </div>
-            </div>
+              <>
+              <Card style={{ width: "18rem", display: "inline-block" ,backgroundColor:"#f8f8f8",margin:"120px",marginLeft:"40px",marginRight:"105px",marginBottom:"-20px",padding:"10px"}}>
+              <Card.Img variant="top" src={cartitems.image} style={{height:"300px"}}/>
+              <Card.Body>
+                <Card.Title>{cartitems.name} {cartitems.price}</Card.Title>
+                
+              </Card.Body>
+            </Card>
+               
+                </>
           );
         })}
       </div>
     </div>
   );
+  
 };
 export default Cart;
